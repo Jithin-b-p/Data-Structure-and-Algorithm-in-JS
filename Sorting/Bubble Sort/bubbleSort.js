06d725301a670a7b6ea6705468a8d81b2166d307
unknown <jithinbp007@gmail.com>
2023-11-08 23:33:41 +0530

    bubble sort

--- Sorting/Bubble Sort/bubbleSort.js
+++ Sorting/Bubble Sort/bubbleSort.js
@@ -1,18 +1,19 @@
 function bubbleSort(array) {
-  let isSwapped = false;
-  for(let i = 0; i < array.length; i++) {
-    for(let j = 0; j < array.length - i - 1; j++) {
-      if(array[j] > array[j + 1]) {
+  let isSwapped;
+  for (let i = 0; i < array.length; i++) {
+    isSwapped = false;
+    for (let j = 0; j < array.length - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
         let temp = array[j];
         array[j] = array[j + 1];
         array[j + 1] = temp;
         isSwapped = true;
       }
     }
-    if(isSwapped) break;
+    if (!isSwapped) break;
   }
 }
 
-const array = [5, 2, 10, 2, 1, 0];
+const array = [24, 18, 38, 43, 14, 40, 1, 54];
 bubbleSort(array);
 console.log(array);
